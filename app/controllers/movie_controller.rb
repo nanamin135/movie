f7bc0848be3b1dcb368f0276ba7911a75d801d32
nanamin135 <nanamin9386@gmail.com>
2021-07-09 21:32:21 +0900

    movie

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,6 +1,21 @@
 class MovieController < ApplicationController
     def movie
     end
+    def create
+        #render plain: params[:movie].inspect
+        #raise params.inspect
+        @movie = Movie.new(anime_params)
+        @movie.save 
+        redirect_to movie_path(@movie)
+    end
     def new 
     end
+    def movie
+    end
+    def show 
+        @Anime.find params[:id]
+    end
+    def anime_params
+        params.require(:anime).permit(:title,:text)
+    end 
 end
